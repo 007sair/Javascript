03ac3d32f5aa2a501343880c7796071519cb2dba
longchan <longchan@mia.com>
2016-06-30 15:07:13 +0800

    update

--- hello.js
+++ hello.js
@@ -1,5 +1,25 @@
-function toZero(n) {
-	return n<10?n='0'+n:n;
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2016-01-14 20:52:10
+ * @version $Id$
+ */
+
+
+
+console.time('haha');
+
+var arr = [];
+var count = 0;
+
+function aa(){
+	while(count < 100000){
+		arr.push(count);
+		count++
+	}
+	return arr;
 }
 
-console.log(toZero(2), typeof toZero(2));
+console.log(aa())
+
+console.timeEnd('haha')
--- test.js
+++ test.js
@@ -1,7 +1,8 @@
- var t = '{"firstName": "cyra", "lastName": "richardson", "address": { "streetAddress": "1 Microsoft way", "city": "Redmond", "state": "WA", "postalCode": 98052 },"phoneNumbers": [ "425-777-7777","206-777-7777" ] }';
- // var jsonobj = eval("(" + t + ")");
- var jsonobj = JSON.parse(t);
- console.log(jsonobj.firstName);
- console.log(jsonobj.lastName);
- console.log(jsonobj["address"]);
- console.log(jsonobj.address.streetAddress);
+function Fn (name) {
+	this.get = function(){
+		console.log(name)
+	}
+}
+var a = new Fn('long');
+
+a.get();
